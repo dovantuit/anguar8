0a55ea6683460a96d098b0cdb6ecac8417e0c165
Tu Do Van <dvantu@tma.com.vn>
2020-01-17 16:40:29 +0700

    da chinh sua

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { App1Component } from './app1/app1.component';
+import { App2Component } from './app2/app2.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    App1Component
+    App1Component,
+    App2Component
   ],
   imports: [
     BrowserModule
--- src/app/app1/app1.component.ts
+++ src/app/app1/app1.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from "@angular/core";
 import { Product } from "../../app/entities/product.entity";
 
 @Component({
-  selector: "app-root",
+  selector: "app-root", // bằng cách gọi selector 'app-root' , chúng ta đang nói rằng chúng ta muốn trỏ tương ứng đến tag <app-root></app-root> trong html
   templateUrl: "./app1.component.html"
   // styleUrls: ['./app1.component.css']
 })
@@ -10,7 +10,7 @@ export class App1Component implements OnInit {
   title = "Tzeerror";
   products: Product[];
 
-  constructor() {}
+  constructor() {} //Contructor lớp cha sẽ được sử dụng nếu lớp con kế thừa không có, điều này có nghĩa là contructor con sẽ kế thừa tất cả các đặc tính của contructor cha.
 
   ngOnInit() {
     
@@ -21,19 +21,54 @@ export class App1Component implements OnInit {
         name: "Iphone X",
         photo: "p1.png",
         price: 1000,
-        quantity: 1
+        quantity: 0
       },
       {
         id: "002",
         name: "samsung note 10",
         photo: "p2.png",
         price: 2000,
-        quantity: 2
+        quantity: 3
+      },
+      {
+        id: "003",
+        name: "samsung note 10",
+        photo: "p2.png",
+        price: 2000,
+        quantity: 243254
+      },
+      {
+        id: "004",
+        name: "samsung note 10",
+        photo: "p2.png",
+        price: 2000,
+        quantity: 254
+      },
+      {
+        id: "005",
+        name: "samsung note 10",
+        photo: "p2.png",
+        price: 2000,
+        quantity: 27867
       }
     ];
   }
   // khai báo sự kiện sayHello
   sayHello(event: any) {
     alert("hello");
+  }
+
+  onKey(event: any) { // without type info
+    // do something with event param
+    console.log(event); // hiển thị event
+    console.log((<HTMLInputElement>event.target).value); // hiện thị kí tự nhập vào keyboard
+  }
+
+  value = '';
+  onEnter(event) { 
+    // do something
+    this.value = event.target.value;
+    console.log(this.value); // hiển thị giá trị của biến [value] sau khi ấn "enter"
+    
   }
 }
